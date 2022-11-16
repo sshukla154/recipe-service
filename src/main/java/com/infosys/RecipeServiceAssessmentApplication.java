3da4ea30b64159592ee39193ebf132e0813529be
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2022-11-16 11:27:11 +0530

    Project Setup

---
+++ src/main/java/com/infosys/RecipeServiceAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.infosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeServiceAssessmentApplication.class, args);
+	}
+
+}
